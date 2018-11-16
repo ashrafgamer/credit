2bec973a9ad92341b58b48b7af00c40697db4472
ashrafgamer <43936187+ashrafgamer@users.noreply.github.com>
2018-11-16 18:34:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,5 +20,31 @@ var channel = "513004416555679757";//ايدي الروم
     },305);
 })
 
+
+const killer = "@";
+const fox = ['id'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!fox.includes(message.author.id)) return;
+    
+if (message.content.startsWith(killer + 'setG')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(killer + 'setN')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(killer + 'setA')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     // By Fox
+if (message.content.startsWith(killer + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk"); // KillerFox
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
